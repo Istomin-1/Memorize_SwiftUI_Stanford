be82e4942e5343b8c01c455d2f0c74c82a5de70d
Istomin-1 <74534747+Istomin-1@users.noreply.github.com>
2021-06-20 17:51:07 +0500

    Added GeometryReader to interface for correct display of card size. Constants moved to a separate structure.

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -8,21 +8,23 @@
 import SwiftUI
 
 class EmojiMemoryGame: ObservableObject {
-    static let emojis = ["🚜", "🛴", "🚆", "🏎", "🚇", "🚛", "⛵️", "🚖", "🛩", "🛳", "🚁", "🛶"]
+    typealias Card = MemoryGame<String>.Card
     
-    static func createMemoryGame() -> MemoryGame<String> {
+    private static let emojis = ["🚜", "🛴", "🚆", "🏎", "🚇", "🚛", "⛵️", "🚖", "🛩", "🛳", "🚁", "🛶"]
+    
+    private static func createMemoryGame() -> MemoryGame<String> {
         MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in emojis[pairIndex] }
     }
     
-    @Published private var model: MemoryGame<String> = createMemoryGame()
+    @Published private var model = createMemoryGame()
     
-    var cards: [MemoryGame<String>.Card] {
+    var cards: [Card] {
         return model.cards
     }
     
 //    MARK: - Intent(s)
     
-    func choose(_ card: MemoryGame<String>.Card) {
+    func choose(_ card: Card) {
         model.choose(card)
     }
 }
---
+++ Memorize/EmojiMemoryGameView.swift
@@ -0,0 +1,91 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Mikhail on 18.06.2021.
+//
+
+import SwiftUI
+
+struct EmojiMemoryGameView: View {
+   @ObservedObject var game: EmojiMemoryGame
+  
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 85))]) {
+                ForEach(game.cards) { card in
+                    CardView(card: card).aspectRatio(2/3, contentMode: .fit)
+                        .onTapGesture {
+                            game.choose(card)
+                    }
+                }
+            }
+        }
+        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+        .padding(.horizontal)
+    }
+}
+     
+struct CardView: View {
+    let card: EmojiMemoryGame.Card
+    
+    var body: some View {
+        GeometryReader{ geometry in
+            ZStack {
+                let shape = RoundedRectangle(cornerRadius: DrawingConstans.cornerRadius)
+                if card.isFaceUp {
+                    shape.fill().foregroundColor(.white)
+                    shape.strokeBorder(lineWidth: DrawingConstans.lineWidth)
+                    Text(card.content).font(font(in: geometry.size))
+                } else if card.isMatched {
+                    shape.opacity(0)
+                } else {
+                    shape.fill()
+                }
+            }
+        }
+    }
+    
+    private func font(in size: CGSize) -> Font {
+        Font.system(size: min(size.width, size.height) * DrawingConstans.fontScale)
+    }
+    
+    private struct DrawingConstans {
+        static let cornerRadius: CGFloat = 20
+        static let lineWidth: CGFloat = 3
+        static let fontScale: CGFloat = 0.8
+     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        let game = EmojiMemoryGame()
+        EmojiMemoryGameView(game: game)
+            .preferredColorScheme(.dark)
+        EmojiMemoryGameView(game: game)
+            .preferredColorScheme(.light)
+    }
+}
