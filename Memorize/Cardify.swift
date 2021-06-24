f10eace335b367a228f5a7fc5001a473aff846fd
Istomin-1 <74534747+Istomin-1@users.noreply.github.com>
2021-06-24 22:09:30 +0500

    Added animations: dealing cards, restarting, rearranging and flipping cards.

--- Memorize/Cardify.swift
+++ Memorize/Cardify.swift
@@ -7,22 +7,33 @@
 
 import SwiftUI
 
-struct Cardify: ViewModifier {
-    var isFaceUp: Bool
+struct Cardify: AnimatableModifier {
+    
+    init(isFaceUp: Bool) {
+        rotation = isFaceUp ? 0 : 180
+    }
+    
+    var animatableData: Double {
+        get { rotation }
+        set { rotation = newValue }
+    }
+    
+    var rotation: Double
     
     func body(content: Content) -> some View {
         ZStack {
             
             let shape = RoundedRectangle(cornerRadius: DrawingConstans.cornerRadius)
-            if isFaceUp {
+            if rotation < 90 {
                 shape.fill().foregroundColor(.white)
                 shape.strokeBorder(lineWidth: DrawingConstans.lineWidth)
             } else {
                 shape.fill()
             }
             content
-                .opacity(isFaceUp ? 1 : 0)
+                .opacity(rotation < 90 ? 1 : 0)
         }
+        .rotation3DEffect(Angle.degrees(rotation), axis: (0, 1, 0))
     }
     private struct DrawingConstans {
         static let cornerRadius: CGFloat = 10
